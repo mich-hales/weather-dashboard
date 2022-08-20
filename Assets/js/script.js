c2f10c6df039d323e2f344d86eb8d3eb89f7c545
mich-hales <michellehales93@gmail.com>
2022-08-20 17:48:09 -0600

    adjusting how to get api and adding moment date

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,19 +4,75 @@ let cityInputEl = document.querySelector('.input-city-element');
 let searchResultsContainer = document.querySelector('.search-results-container');
 let featuredCityContainer = document.querySelector('.featured-city-container');
 let fiveDayForecastContainer = document.querySelector('.five-day-forecast-container');
+let clearHistory = document.querySelector('#clear-history');
+let searchHistory = document.querySelector('.search-history');
 
 
 // When button is clicked, the api will search the searched city 
-searchButton.addEventListener('submit', function() {
-    var citySearched = cityInputEl.value.trim();
-    var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + citySearched + '&limit=5&appid=d277d11a67875138e278bf921f539c35'
+// searchButton.addEventListener('submit', function() {
+//     var citySearched = cityInputEl.value.trim();
+//     var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + citySearched + '&units=imperial&appid=d277d11a67875138e278bf921f539c35'
     
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => console.log(data))
+//     // fetch request to get latitute and longitude of city searched
+//     fetch(apiUrl).then(function (response) {
+//         response.json()
+//         console.log(response.json());
+//     })
+//     .then(function (data) {
+//         console.log(data)
+
+//         // latitude and longitude coordinates of the city searched
+//         // let latitude = response.coord.lat;
+//         // let longitude = response.coord.lon;
+
+//         // 
+//     })
+
+  
+// })
+
+var currentDate = document.querySelector('.current-date');
+var citySearched = cityInputEl.value.trim();
+var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=denver&units=imperial&appid=d277d11a67875138e278bf921f539c35'
     
-    .catch(err => alert('Invalid city name'));    
+fetch(apiUrl).then(function (response) {
+    return response.json()
+})
+.then(function (data) {
+    console.log(data)
+
+    // longitude and latitude coordinates of the city searched
+    let longitude = data.coord.lon;
+    let latitude = data.coord.lat;
+
+    // variables
+    let city = data.name;
+    var date = moment().format('MM/DD/YYYY');
+    currentDate.textContent = date;
+
 })
 
 
 
+
+
+
+// testing list function
+
+// // when text is input (key add event listener)
+
+
+// fetch(apiUrl)
+// .then(response => response.json())
+// .then(data => console.log(data))
+
+// .catch(err => alert('Invalid city name'));    
+
+
+// // the searched city will pop up on the page when search button is clicked
+// var listItem = document.createElement('li');
+// listItem.classList.add('list-styling');
+// searchHistory.appendChild(listItem);
+// listItem.textContent = 'testing testing'
+
+// console.log('help')
