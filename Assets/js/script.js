8b0601e731caaa2c6df3e76afa738b42b2d9f814
mich-hales <michellehales93@gmail.com>
2022-08-19 20:26:12 -0600

    added submit button, not working...

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,21 +3,20 @@ let searchButton = document.querySelector('.submit-button');
 let cityInputEl = document.querySelector('.input-city-element');
 let searchResultsContainer = document.querySelector('.search-results-container');
 let featuredCityContainer = document.querySelector('.featured-city-container');
-let fiveDayForecastContainer = document.querySelector('.5-day-forecast-container');
+let fiveDayForecastContainer = document.querySelector('.five-day-forecast-container');
 
 
-// current weather
-
-
-
-var citySearched = cityInputEl.value.trim();
-var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + citySearched + '&limit=5&appid=d277d11a67875138e278bf921f539c35'
-
-fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => console.log(data))
-
-.catch(err => alert('Invalid city name'));
+// When button is clicked, the api will search the searched city 
+searchButton.addEventListener('click', function() {
+    var citySearched = cityInputEl.value.trim();
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + citySearched + '&limit=5&appid=d277d11a67875138e278bf921f539c35'
+    
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => console.log(data))
+    
+    .catch(err => alert('Invalid city name'));    
+})
 
 
 
