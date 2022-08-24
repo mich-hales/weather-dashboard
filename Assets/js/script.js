6628f6fcd23f1a39228e4dc4d15739fe9713a74f
mich-hales <michellehales93@gmail.com>
2022-08-24 08:25:01 -0600

    fixed the functions to get them working properly

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,19 +6,29 @@ let featuredCityContainer = document.querySelector('.featured-city-container');
 let fiveDayForecastContainer = document.querySelector('.five-day-forecast-container');
 let clearHistory = document.querySelector('#clear-history');
 let searchHistoryList = document.querySelector('.search-history');
+let cityForm = document.querySelector('#city-form');
+
 
 
 // array for search history
-var searchHistory = [];
+let searchHistory = [];
+console.log(searchHistory)
+
 // save search history 
 let formSubmitHandler = function (event) {
     event.preventDefault();
 
     // the user input / searched city
     var citySearched = cityInputEl.value.trim();
+    console.log(citySearched)
+
+    // searchHistory array is not working . . . push isn't working? ? ? 
+
 
     // local storage
     if (citySearched) {
+        console.log(searchHistory);
+        console.log(citySearched);
         searchHistory.push(citySearched);
         localStorage.setItem('search-history', JSON.stringify(searchHistory));
         let searchHistoryBtn = document.createElement('button');
@@ -47,6 +57,8 @@ var apiKey = '300ba1bc4c70b9982f60158a745b8368';
 let weatherInfo = function(citySearched) {
     var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + citySearched + '&units=imperial&appid=300ba1bc4c70b9982f60158a745b8368'
 
+    console.log(apiUrl);
+
     fetch(apiUrl).then(function (cityResponse) {
         return cityResponse.json()
     })
@@ -54,8 +66,8 @@ let weatherInfo = function(citySearched) {
         console.log(cityResponse)
     
         // longitude and latitude coordinates of the city searched
-        let longitude = data.coord.lon;
-        let latitude = data.coord.lat;
+        let longitude = cityResponse.coord.lon;
+        let latitude = cityResponse.coord.lat;
     
         // variables
         let city = cityResponse.name;
@@ -85,9 +97,15 @@ let weatherInfo = function(citySearched) {
         //     headers: new Headers({"content-type": "application/json"}),
         //     mode: "no-cors",
         // };
+
+
+        // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+        // this is the 5 day forecast api . . . 
+
+        console.log('https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude +  '&units=imperial&appid=d277d11a67875138e278bf921f539c35')
         
         // taking latitude and longitutde from city searched and returning a fetch request
-        return fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude +  '&units=imperial&appid=d277d11a67875138e278bf921f539c35')
+        return fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude +  '&units=imperial&appid=d277d11a67875138e278bf921f539c35')
     })
     .then(function(response) {
         return response.json();
@@ -107,26 +125,89 @@ const displayWeather = function (cityWeather) {
     // temperature
     let temperature = document.createElement('p');
     temperature.classList.add('current-temperature');
-    temperature.textContent = 'Temperature: ' + cityWeather.main.temp + '°F';
+    temperature.textContent = 'Temperature: ' + cityWeather.list[0].main.temp + '°F';
     // temperature.textContent = 'Temperature: ' + city.current.temp.toFixed(1) + '°F';
     featuredCityContainer.appendChild(temperature);
 
     // humidity
     let humidity = document.createElement('p');
     humidity.classList.add('humidity');
-    humidity.textContent = 'Humidity: ' + cityWeather.main.humidity + '%';
+    humidity.textContent = 'Humidity: ' + cityWeather.list[0].main.humidity + '%';
     // humidity.textContent = 'Humidity: ' + city.current.humidity + '%';
     featuredCityContainer.appendChild(humidity);
 
     // wind
     let wind = document.createElement('p');
-    wind.textContent = 'Wind Speed: ' + cityWeather.wind.speed + 'MPH';
+    wind.textContent = 'Wind Speed: ' + cityWeather.list[0].wind.speed + 'MPH';
     // wind.textContent = 'Wind Speed: ' + city.current.wind_speed + 'MPH';
     featuredCityContainer.appendChild(wind);
 
+
+
+
+    // not sure what to call in the api . . . 
+    var dailyForecast = cityWeather.list;
+    console.log(dailyForecast);
+
+    // // 5 day forecast 
+    for (let i = 3; i < dailyForecast.length; i = i + 8) {
+        var temp = dailyForecast[i].main.temp;
+        dailyForecast[i].dt_txt;
+        console.log(dailyForecast[i].main.temp)
+        console.log(dailyForecast[i].dt_txt)
+
+        var test = document.createElement('p');
+        test.textContent = temp;
+        fiveDayForecastContainer.appendChild(test);
+    }  
+        
+    
+    
+    
+    
+    // is dt_txt going to get me the date?
+        // forecastDate[i] = dailyForecast.list[i].dt_txt;
+        // forecastIcon[i] = dailyForecast[i].weather[0].icon;
+   
+        // forecastWind[i] = dailyForecast[i].wind.speed;
+        // forecastHum[i] = dailyForecast[i].main.humidity;
+
+        
+
+
+
+    //     displayElement.innerHTML = '<p>' + date + '</p>' +
+    //     '<p>' + showIcon +'</p>' +
+    //     '<p>Temp: ' + dailyForecast[i].temp.day.toFixed(1) + '°F</p>' +
+    //     '<p>Humidity: ' + dailyForecast[i].humidity + '%</p>' +
+    //     '<p>Wind: ' + dailyForecast[i].wind_speed +'MPH</p>' 
+    // // append to page
+    // fiveDayForecastContainer.appendChild(displayElement); 
+
+
+
+    // var today = new Date();
+    // console.log(today);
+
+
+    //     // displays date -- for the next 5 days
+    //     var date = (today.getMonth() + 1) + '/' + (today.getDate() + i + 1) + '/' + today.getFullYear();
+    //     // displays icon for current condition (URL is http://openweathermap.org/img/wn/10d@2x.png -- example code on openweather to retrieve icons)
+    //     var showIcon = "<img src='http://openweathermap.org/img/wn/" + icon + "@2x.png'/>"
+    //     var displayElement = document.createElement('div');
+    
+}
+
+
+
+
+    // IF I'm able to use the opencall api, I looked up the data and figured this is how it would work. . . but the api isn't working still
+    // still need to test it to see if it works properly 
+
+
     // uv-index
     // let uvIndex = document.createElement('p');
-    // let uvIndexVal = city.current.uvi.toFixed(1);
+    // let uvIndexVal = cityWeather.current.uvi.toFixed(1);
     // if (uvIndexVal >= 0) {
     //     uvIndex.classList.add('uv-green');
     // } else if (uvIndexVal >=3) {
@@ -143,10 +224,10 @@ const displayWeather = function (cityWeather) {
 
 
 
+// this is how I would be able to present the 5 day forecast, but the opencall api still isn't working and not sure how to do it without it.
+// haven't been able to test it cuz the api isn't working so still not sure if it will work properly . . . 
 
-
-
-    // var dailyForecast = city.daily;
+    // var dailyForecast = cityWeather.daily;
     // var today = new Date();
 
     // // // 5 day forecast 
@@ -166,7 +247,10 @@ const displayWeather = function (cityWeather) {
     //     // append to page
     //     fiveDayForecastContainer.appendChild(displayElement); 
     // }  
-}
+
+
+
+
 
 
 
@@ -200,13 +284,25 @@ let historyButtonSearch = function (event) {
     }
 }
 
+
+// will this properly remove the buttons from page? 
+
+
+
+
+// show clear history buttons
+
 // clear search history from local storage
 let clearHistoryButton = function () {
     localStorage.removeItem('search-history');
 }
 
+
+// why does submit button not work?
+
+
 // when city is searched and the search button is clicked, will call the formSubmitHandler function which initiates everything
-searchButton.addEventListener('submit', formSubmitHandler);
+cityForm.addEventListener('submit', formSubmitHandler);
 // when a previously searched city's button is clicked on, will call the historyButtonSearch function
 searchHistoryList.addEventListener('click', historyButtonSearch);
 // when the clear history's button is clicked on, will call ClearHistoryButton function
